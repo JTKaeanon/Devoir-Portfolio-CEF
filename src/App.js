7471fd2cae4534b4cd6af92e22bbe5686e3195e9
Jessie Texier <jessitexier8@gmail.com>
2025-11-11 17:48:40 +0100

    début pages mentions legales

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './assets/App.css';
 
 
 
+
 function App() {
   return (
     <>
--- src/routes/Legales/Legales.js
+++ src/routes/Legales/Legales.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Mentions from '../../components/Accordion/Accordion'
 
 function Legales() {
   return (
+    <>
     <div>
       <h1>Mentions Légales</h1>
     </div>
+    <Mentions/>
+    </>
   )
 }
 
