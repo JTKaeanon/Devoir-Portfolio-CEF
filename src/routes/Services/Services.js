f4758f72bbea5cdd9df6b3c42c874a6586b5f20e
Jessie Texier <jessitexier8@gmail.com>
2025-11-12 17:34:44 +0100

    Pages Services finis + css hero

--- src/routes/Services/Services.js
+++ src/routes/Services/Services.js
@@ -5,10 +5,11 @@ import '../../assets/Services.css'
 function Services() {
   return (
     <>
-    <div className='page-services'>
-      <h1 className='services-titre'>Mon offre de services</h1>
-      <ServicesCards />
-    </div>
+      <section className='hero'></section>
+      <div className='page-services'>
+        <h1 className='services-titre'>Mon offre de services</h1>
+        <ServicesCards />
+      </div>
     </>
   )
 }
